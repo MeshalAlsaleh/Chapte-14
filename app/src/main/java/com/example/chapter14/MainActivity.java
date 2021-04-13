4482ccedad7a9f02dbe45cde2a5cf480e3f87046
MeshalAlsaleh <79575104+MeshalAlsaleh@users.noreply.github.com>
2021-04-14 01:09:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chapter14/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.chapter14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MyAndroid", "onCreate");
+
+        Toasty.info(getBaseContext(), "This is an info toast.",
+                Toast.LENGTH_SHORT, true).show();
+        Toasty.error(getBaseContext(), "This is an error toast.",
+                Toast.LENGTH_SHORT, true).show();
+        Toasty.success(getBaseContext(), "Success.", Toast.LENGTH_SHORT,
+                true).show();
+        Toasty.normal(getBaseContext(), "Normal toast").show();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("MyAndroid", "onStart");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("MyAndroid","onResume");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("MyAndroid","onPause");
+    }
+    public void onStop() {
+        super.onStop();
+        Log.d("MyAndroid","onStop");
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyAndroid","onDestroy");
+    }
+}
